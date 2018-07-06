0138e56a260746de514224bf9496ffd0fd3cd481
Jed Peek <jedpeek@yahoo.com>
2018-07-06 16:21:38 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,9 +26,7 @@ def can_be_created_in_a_block(args = {})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
-    m.director = args[:director]
-    m.lead = args[:lead]
-    m.in_theaters = args[:in_theaters]
+
     m.save
   end
   end
