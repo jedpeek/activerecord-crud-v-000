9d6b129b17c799bda41505693a4ee9aa2734d29e
Jed Peek <jedpeek@yahoo.com>
2018-07-06 16:22:07 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,11 +26,12 @@ def can_be_created_in_a_block(args = {})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
-
+    m.director = args[:director]
+    m.lead = args[:lead]
+    m.in_theaters = args[:in_theaters]
     m.save
+    end
   end
-  end
-end
 
 def can_get_the_first_item_in_the_database
   __
